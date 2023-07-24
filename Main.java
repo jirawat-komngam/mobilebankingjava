c4c3957202556d65ccd957f140c0fc46759daaad
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-24 21:52:26 +0700

    add UI

--- Main.java
+++ Main.java
@@ -1,3 +1,14 @@
+class Program {
+    public void start() {
+        System.out.println("mobile banking");
+        System.out.println("1 create account");
+        System.out.println("2 account status");
+        System.out.println("3 deposit/withdraw");
+        System.out.println("3 4 transfer");
+    }
+
+}
+
 public class Main {
 
     public static void main(String[] args) {
