e36ed1558b368ecd475c5d2706cdc7ceb0c05de6
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-24 22:52:49 +0700

    add feature to welcomeofficer

--- WelcomeOfficer.java
+++ WelcomeOfficer.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 public class WelcomeOfficer implements Officer {
+    private Scanner ear;
+    private CreateAccountOfficer createAccountOfficer;
+
+    WelcomeOfficer(CreateAccountOfficer realCreateAccountOfficer) {
+        createAccountOfficer = realCreateAccountOfficer;
+    }
+
     public void call() {
         System.out.println("mobile banking");
         System.out.println("1 create account");
         System.out.println("2 account status");
         System.out.println("3 deposit/withdraw");
         System.out.println("4 transfer");
+    }
+
+    public void callOtherOfficer(String userRequest) {
+        if (userRequest.equals("1")) {
+
+        }
     }
 }
