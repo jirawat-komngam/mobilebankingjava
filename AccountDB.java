7b6146cdb0bffa488b60be0096d5789f39c5bac5
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-24 22:34:24 +0700

    add detail accountDB

---
+++ AccountDB.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class AccountDB {
+    private List<AccountPOJO> accountList;
+
+    public List<AccountPOJO> getAccountList() {
+        return accountList;
+    }
+
+    public void setAccountList(List<AccountPOJO> accountList) {
+        this.accountList = accountList;
+    }
+
+}
