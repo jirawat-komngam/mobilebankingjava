18c057084138dd35c7d46b48d71de4d6c7296d06
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-24 22:33:02 +0700

    add feature to pojo

---
+++ AccountPOJO.java
@@ -0,0 +1,29 @@
+public class AccountPOJO {
+    private String accountName;
+    private String accountPassword;
+    private Double accountBalance;
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public String getAccountPassword() {
+        return accountPassword;
+    }
+
+    public void setAccountPassword(String accountPassword) {
+        this.accountPassword = accountPassword;
+    }
+
+    public Double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(Double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+}
