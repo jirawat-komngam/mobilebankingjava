3f8d63fe29f40c86acb4f9dd73fa3a304dccb8bb
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-24 22:23:33 +0700

    add createAccountOfficer

---
+++ CreateAccountOfficer.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CreateAccountOfficer implements Officer {
+    private Scanner ear;
+
+    CreateAccountOfficer() {
+        ear = new Scanner(System.in);
+    }
+
+    public void call() {
+        System.out.println("-create account");
+        System.out.println("account name");
+        String accountNameInput = ear.nextLine();
+        System.out.println("account password");
+        String accountPasswordInput = ear.nextLine();
+        System.out.println("deposit");
+        Double accountMoneyInput = Double.parseDouble(ear.nextLine());
+        System.out.println("account created");
+    }
+}
