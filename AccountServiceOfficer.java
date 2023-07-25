116333a01f1aa4951a7b65c83d4238b5b991da42
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-25 16:02:17 +0700

    add transfer feature

--- AccountServiceOfficer.java
+++ AccountServiceOfficer.java
@@ -15,6 +15,7 @@ public class AccountServiceOfficer {
         System.out.println("balance : " + accountBalance + " baht");
         System.out.println("1 - deposit");
         System.out.println("2 - withdraw");
+        System.out.println("3 - transfer");
         String userChoiceValues = ear.nextLine();
         userChoice(userChoiceValues, accountName, accountBalance);
     };
@@ -65,7 +66,45 @@ public class AccountServiceOfficer {
 
             }
 
-        }
+        } else if (choice.equals("3")) {
+            System.out.println("transfer");
+            System.out.println("amount of money");
+            Double moneyAmount = Double.parseDouble(ear.nextLine());
+            System.out.println("withDraw : " + moneyAmount + " baht");
+            System.out.println("write reciever name");
+            String recieverNameInput = ear.nextLine();
+            System.out.println("confirm??");
+            String confirmation = ear.nextLine();
+            for (AccountPOJO eachAccount : accountDB.getAccountList()) {
+                if (confirmation.equals("yes")) {
+                    if (accountName.equals(recieverNameInput)) {
+                        System.out.println("permission is denied");
+                    } else {
+                        if (eachAccount.getAccountInfo().containsKey(recieverNameInput)) {
+                            System.out.println("tranfer  complete");
+                            System.out.println("transfer " + moneyAmount + "baht");
+                            if (moneyAmount <= accountBalance) {
+                                Double newBalance = accountBalance - moneyAmount;
+                                Double recieverBalance = accountBalance + moneyAmount;
+                                System.out.println("balance :" + newBalance + "baht");
+                                if (eachAccount.getAccountInfo().containsKey(accountName)) {
+                                    eachAccount.setAccountBalance(newBalance);
+                                }
+                                if (eachAccount.getAccountInfo().containsKey(recieverNameInput)) {
+                                    eachAccount.setAccountBalance(recieverBalance);
+                                }
+                            }
+                        } else {
+                            System.out.println("your money is not enough");
+                        }
+                    }
 
+                } else {
+                    System.out.println("not found this account");
+                }
+            }
+
+        }
     }
+
 }
