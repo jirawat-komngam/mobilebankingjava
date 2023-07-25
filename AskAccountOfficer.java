eed72ff64dd272ce44dbe443096742bc032ace90
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-25 09:04:08 +0700

    add login features

--- AskAccountOfficer.java
+++ AskAccountOfficer.java
@@ -12,10 +12,18 @@ public class AskAccountOfficer implements Officer {
     public void call() {
         System.out.println("login success");
         System.out.println("account name");
+        String realPasswordInput = "";
         String accountNameInput = ear.nextLine();
-        System.out.println("account password");
-        String accountPasswordInput = ear.nextLine();
+        for (AccountPOJO eachAccount : accountDB.getAccountList()) {
+            if (eachAccount.getAccountInfo().containsKey(accountNameInput) == true) {
+                System.out.println("account password");
+                String accountPasswordInput = ear.nextLine();
+                realPasswordInput = eachAccount.getAccountInfo().get(accountNameInput);
+                if (accountPasswordInput.equals(realPasswordInput)) {
+                    System.out.println("login success");
+                }
+            }
+        }
 
-        System.out.println("login success");    
     }
 }
