8b6bb8e2f498282848fe4b9e7063b7053fbcc68b
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-25 15:00:14 +0700

    fix accountservice

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ class Program {
     public void start() {
         AccountDB database = new AccountDB(new ArrayList<>());
         CreateAccountOfficer newCreateAccountOfficer = new CreateAccountOfficer(database);
-        AccountServiceOfficer newAccountServiceOfficer = new AccountServiceOfficer(database);
+        AccountServiceOfficer newAccountServiceOfficer = new AccountServiceOfficer();
         AskAccountOfficer newAskAccountOfficer = new AskAccountOfficer(database, newAccountServiceOfficer);
         WelcomeOfficer newWelcomeOfficer = new WelcomeOfficer(newCreateAccountOfficer, newAskAccountOfficer);
         Bank newBank = new Bank(newWelcomeOfficer);
