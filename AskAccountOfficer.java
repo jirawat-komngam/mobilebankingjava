dfe0dba314c07f357f036ee210d3b55198293ee7
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-25 08:24:11 +0700

    add features to askaccount

--- AskAccountOfficer.java
+++ AskAccountOfficer.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class AskAccountOfficer implements Officer {
+    private Scanner ear;
+    private AccountDB accountDB;
+
+    AskAccountOfficer(AccountDB realAccountDB) {
+        ear = new Scanner(System.in);
+        this.accountDB = realAccountDB;
+    }
+
     public void call() {
         System.out.println("login success");
-        System.out.println("login success");
-        System.out.println("login success");
+        System.out.println("account name");
+        String accountNameInput = ear.nextLine();
+        System.out.println("account password");
+        String accountPasswordInput = ear.nextLine();
 
-        System.out.println("login success");
+        System.out.println("login success");    
     }
 }
--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ class Program {
     public void start() {
         AccountDB database = new AccountDB(new ArrayList<>());
         CreateAccountOfficer newCreateAccountOfficer = new CreateAccountOfficer(database);
-        AskAccountOfficer newAskAccountOfficer = new AskAccountOfficer();
+        AskAccountOfficer newAskAccountOfficer = new AskAccountOfficer(database);
         WelcomeOfficer newWelcomeOfficer = new WelcomeOfficer(newCreateAccountOfficer, newAskAccountOfficer);
         Bank newBank = new Bank(newWelcomeOfficer);
         newBank.enterBank();
