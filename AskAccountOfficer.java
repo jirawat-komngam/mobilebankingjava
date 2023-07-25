fc195035d8218bc375ab6e3259087138b565afc4
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-25 08:20:36 +0700

    add askAccountOfficer

---
+++ AskAccountOfficer.java
@@ -0,0 +1,9 @@
+public class AskAccountOfficer implements Officer {
+    public void call() {
+        System.out.println("login success");
+        System.out.println("login success");
+        System.out.println("login success");
+
+        System.out.println("login success");
+    }
+}
