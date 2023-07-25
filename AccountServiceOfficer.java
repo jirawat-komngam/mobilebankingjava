eedf11dcbe3b49ed862fec8a22da9ae42ea0cbb2
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-25 14:01:09 +0700

    add serviceofficer detail

--- AccountServiceOfficer.java
+++ AccountServiceOfficer.java
@@ -1,3 +1,10 @@
-public class AccountServiceOfficer {
+public class AccountServiceOfficer implements Officer {
+    public void call() {
 
+        System.out.println("welcome : kuy");
+        System.out.println("balance : 1000 baht");
+        System.out.println("1 - deposit");
+        System.out.println("2 - withdraw");
+
+    };
 }
