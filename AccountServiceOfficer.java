920d2d8eefb074bf0620cbc839f1bcfc809e6de2
jirawat-komngam <jirawat.komn@gmail.com>
2023-07-25 14:15:33 +0700

    add funtional detail

--- AccountServiceOfficer.java
+++ AccountServiceOfficer.java
@@ -22,6 +22,32 @@ public class AccountServiceOfficer implements Officer {
         if (choice.equals("1")) {
             System.out.println("deposit");
             System.out.println("amount of money");
+            Double moneyAmount = Double.parseDouble(ear.nextLine());
+            System.out.println("deposit : \"500 baht\"\r\n" + //
+                    "");
+            System.out.println("confirm??");
+            String confirmation = ear.nextLine();
+            if (confirmation.equals("yes")) {
+                System.out.println("deposit complete");
+                System.out.println("deposit 500 baht");
+                System.out.println("balance 1500 baht");
+            }
+
+        } else if (choice.equals("2")) {
+            System.out.println("withdraw");
+            System.out.println("amount of money");
+            Double moneyAmount = Double.parseDouble(ear.nextLine());
+            System.out.println("withdraw : \"500 baht\"\r\n" + //
+                    "");
+            System.out.println("confirm??");
+            String confirmation = ear.nextLine();
+            if (confirmation.equals("yes")) {
+                System.out.println("withdraw complete");
+                System.out.println("withdraw 500 baht");
+                System.out.println("balance 1000 baht");
+            }
+
         }
+
     }
 }
